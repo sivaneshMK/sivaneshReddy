b61721502bdcddeaa10762728529a32c8ccabafc
sivaneshMK <40260943+sivaneshMK@users.noreply.github.com>
2019-07-11 13:31:11 +0530

    Add files via upload

--- Selenium2/src/MultiselectListbox.java
+++ Selenium2/src/MultiselectListbox.java
@@ -12,21 +12,41 @@ public class MultiselectListbox {
 	public static void main(String[] args) throws InterruptedException {
 		
 		System.setProperty("webdriver.chrome.driver",
-				 "C:\\Users\\Faculty\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
-			WebDriver driver= new ChromeDriver();
-			driver.get("https://www.monsterindia.com/");
-			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-			driver.findElement(By.className("main-nav-link")).click();
-			
-			WebElement e1= driver.findElement(By.xpath("//span[text()='Industry']"));
-			Thread.sleep(3000);
-			Select s1 = new Select(e1);
-			
-			s1.selectByIndex(0);
-			s1.selectByIndex(2);
+				"C:\\Users\\Faculty\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+	     WebDriver driver= new ChromeDriver();
+		
+	     driver.get("http://mukesh-pc/login.do");
+	     driver.findElement(By.name("username")).sendKeys("admin");
+	     
+	     driver.findElement(By.name("pwd")).sendKeys("manager");
+	     
+	     driver.findElement(By.cssSelector("input[type='submit']")).click();
 	
-	
-	
+	     driver.findElement(By.linkText("Reports")).click();
+	     
+	     WebElement users=driver.findElement(By.name("users"));
+	     
+	     Select s1= new Select(users);
+	     
+	     s1.selectByIndex(0);
+	     Thread.sleep(3000);
+	     s1.selectByValue("3");
+	     Thread.sleep(3000);
+	     s1.selectByVisibleText("kumar, suresh (admin4)");
+	     Thread.sleep(3000);
+	     s1.deselectAll();
+	     Thread.sleep(3000);
+	     s1.selectByIndex(0);
+	     Thread.sleep(3000);
+	     s1.selectByValue("3");
+	     Thread.sleep(3000);
+	     s1.selectByVisibleText("kumar, suresh (admin4)");
+	     Thread.sleep(3000);
+	     s1.deselectByIndex(1);
+	     Thread.sleep(3000);
+	     s1.deselectByValue("4");
+	     Thread.sleep(3000);
+	     s1.deselectByVisibleText("kumar, suresh (admin4)");
 	
 	
 	
